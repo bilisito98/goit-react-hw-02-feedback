3bcf95068c0259e158c9732009be4c56df92efb1
william <wilsher987@gmail.com>
2023-06-13 22:10:22 -0500

    commitfix

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+export default function Wrapper({ children }) {
+    return <div>{children}</div>
+}
+
+Wrapper.propTypes = {
+    children: PropTypes.node,
+}
+
+
+
